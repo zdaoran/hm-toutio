3ce738f2bbb42c41293d5bd9c38ee79e401e8f4b
zdaoran <993370091@qq.com>
2019-08-01 20:34:57 +0800

    更新路由模块

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
 Vue.use(ElementUi)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/router'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+  routes: [{
+    name: 'login',
+    path: '/login',
+    component: Login
+  }]
 })
 
 export default router
